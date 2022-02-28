c4fbaadc529cf67879295c697daa29bee5da32fe
hrz07 <hridoy.developer@yahoo.com>
2022-02-28 17:16:03 +0600

    error msg added

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const allPlayersBox = document.getElementById("search-result-box");
 const singlePlayer = document.getElementById("single-info");
 const spinner = document.getElementById("spinner");
+const validInputMsg = document.getElementById("valid-input-msg");
+const timeOutMsg = document.getElementById("timeout-msg");
 
 const searchBtn = () => {
+    validInputMsg.style.display = "none"; 
+    timeOutMsg.style.display = "none";
     spinner.style.display = "block";
     const input = document.getElementById("input");
     const inputVal = input.value;
@@ -11,35 +15,50 @@ const searchBtn = () => {
         `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${inputVal}`
     )
         .then((res) => res.json())
-        .then((data) => allPlayerFunc(data.player));
+        .then((data) => {
+            if(!isNaN(data.player)){
+                spinner.style.display = "none";
+                validInputMsg.style.display = "block"; 
+            }
+            else{
+                if(data.player != null){
+                    allPlayerFunc(data.player);
+                }else{
+                    setTimeout(timeOutMsgFunc,10000);
+                }     
+            }
+        });
 };
+
+const timeOutMsgFunc = ()=>{
+    spinner.style.display = "none";
+    timeOutMsg.style.display = "block";
+}
 
 const allPlayerFunc = (players) => {
     singlePlayer.innerHTML = "";
     allPlayersBox.innerHTML = "";
-    if (players == null) {
-        spinner.style.display = "block";
-    } else {
-        spinner.style.display = "none";
-        for (const player of players) {
-            const div = document.createElement("div");
-            div.innerHTML = `
+
+    spinner.style.display = "none";
+    for (const player of players) {
+        const div = document.createElement("div");
+        div.innerHTML = `
             
             <div class="card" style="width: 18rem;">
             <img src="${player.strThumb ? player.strThumb : player.strRender
-                }" class="card-img-top" alt="...">
+            }" class="card-img-top" alt="...">
             <div class="card-body">
             <h5 class="card-title">${player.strPlayer}</h5>
             <p class="card-text">${player.strNationality}</p>
                 <a href="#" onclick="details(${player.idPlayer
-                })" class="btn btn-primary">Details</a>
+            })" class="btn btn-primary">Details</a>
             </div>
         </div>
             
             `;
-            allPlayersBox.appendChild(div);
-        }
+        allPlayersBox.appendChild(div);
     }
+
 };
 
 const details = (idPlayer) => {
